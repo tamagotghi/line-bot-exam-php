ec7736a3091d0a40ad197b4fbea78b6787bb5e8d
tamagotghi <p.thestham@gmail.com>
2018-06-11 17:32:08 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,57 +1,6 @@
-<?php // callback.php
-
-require "vendor/autoload.php";
-require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
-
-$access_token = '+I4WzOy1Ji/2+YL1M4xEp1X9pw7F0DTCI4yqKtMt2orieyj++laON3fiCr5yBUTT1E4v+5wnnJrSLhkUz3zJmCNH88ATaik8LHfgCLs3Gyx2MKH05h1eMUV3Rrztd9oWDd4wmsIZCmZ2mZJhKxrxYAdB04t89/1O/w1cDnyilFU=';
-
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['source']['userId'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-		}
-	}
-}
-echo "OK";
 
 <?php
-    $accessToken = "";//copy Channel access token ตอนที่ตั้งค่ามาใส่
+    $accessToken = "+I4WzOy1Ji/2+YL1M4xEp1X9pw7F0DTCI4yqKtMt2orieyj++laON3fiCr5yBUTT1E4v+5wnnJrSLhkUz3zJmCNH88ATaik8LHfgCLs3Gyx2MKH05h1eMUV3Rrztd9oWDd4wmsIZCmZ2mZJhKxrxYAdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
     
     $content = file_get_contents('php://input');
     $arrayJson = json_decode($content, true);
